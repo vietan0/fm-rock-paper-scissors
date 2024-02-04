fc2686c33c71a67426ac01ef79375eb04f9a1d73
Việt An <vietan00892b@gmail.com>
2024-02-04 18:48:49 +0700

    feat: add animation to result message

--- src/ComputerChooseScreen.tsx
+++ src/ComputerChooseScreen.tsx
@@ -27,7 +27,8 @@ export default function ComputerChooseScreen() {
     }
   }, [gameState.choices.computer]);
 
-  const grid2 = 'max-w-xl xs:grid-cols-[repeat(2,_minmax(150px,_1fr))]';
+  const grid2 =
+    'max-w-xl xs:grid-cols-[minmax(150px,_1fr)_0_minmax(150px,_1fr)]';
   const grid3 =
     'max-w-3xl md:gap-x-10 grid-cols-[repeat(2,_minmax(150px,_1fr))] sm:grid-cols-[repeat(3,_minmax(150px,_1fr))]';
 
@@ -48,19 +49,19 @@ export default function ComputerChooseScreen() {
               className="scale-125 sm:scale-150 md:scale-[2.5]"
             />
           </div>
-          {gameState.result && (
-            <div className="order-last col-span-2 h-fit self-end sm:order-none sm:col-auto">
-              <p className="mb-3 text-center text-4xl font-bold uppercase sm:mb-6 md:text-5xl">
-                {resultMsg[gameState.result]}
-              </p>
-              <button
-                onClick={() => dispatch({ type: 'start' })}
-                className="mx-auto block rounded-lg bg-gradient-to-b from-white to-gray-200 px-10 py-3 text-sm uppercase tracking-[.25em] text-dark-text shadow-2xl hover:text-blue-600"
-              >
-                Play Again
-              </button>
-            </div>
-          )}
+          <div
+            className={`${gameState.result ? 'order-last col-span-2 scale-100 sm:order-none' : 'scale-0'} h-fit self-end duration-200 sm:col-auto`}
+          >
+            <p className="mb-3 text-center text-4xl font-bold uppercase sm:mb-6 md:text-5xl">
+              {resultMsg[gameState.result!]}
+            </p>
+            <button
+              onClick={() => dispatch({ type: 'start' })}
+              className="mx-auto block rounded-lg bg-gradient-to-b from-white to-gray-200 px-10 py-3 text-sm uppercase tracking-[.25em] text-dark-text shadow-2xl hover:text-blue-600"
+            >
+              Play Again
+            </button>
+          </div>
           <div className="flex flex-col items-center gap-6 sm:gap-20">
             <p className="h-12 text-center uppercase tracking-widest xs:text-lg">
               The House Picked
